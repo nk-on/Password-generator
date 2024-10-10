4d55da65bc4a28ca0473b9f5c5f14dc38b7c3378
nk-on <johndoe@example.com>
2024-10-10 22:16:01 +0400

    Defining variables in order to manipulate with DOM

---
+++ src/script.js
@@ -0,0 +1,10 @@
+//app should generate random password when user clicks on generate button
+//character length should vary if user changes length variable
+//if user has not checked any option display error
+//app should be able to determine passowrd stregth
+const clipBoardIcon = document.querySelector('.icon');
+const characterLength = document.querySelector('.Characters-length');
+const inputRange = document.querySelector('#input-range')
+const checkBoxes = document.querySelectorAll('#checkBox');
+const levelIndicators = document.querySelectorAll('.level');
+const generateButton = document.querySelector('.generate-button');
