f8c86faffe5bde88df6df4ed3583613e03f08c15
nk-on <johndoe@example.com>
2024-10-12 08:45:48 +0400

    Fix:fixed the bug in particular preventing app from genereting values which are undefined

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@
 //app should be able to determine passowrd stregth
 const clipBoardIcon = document.querySelector('.icon');
 const characterLengthContainer = document.querySelector('.Characters-length');
-const inputRange = document.querySelector('#input-range')
+const inputRange = document.querySelector('#input-range');
 const checkBoxes = document.querySelectorAll('#checkBox');
 const levelIndicators = document.querySelectorAll('.level');
 const generateButton = document.querySelector('.generate-button');
@@ -15,37 +15,45 @@ let passwordLength = undefined;
 // const numbers = '0123456789'.split('');
 // const symbols = '!@#$%^&*()_+[]{}|;:,.<>?'.split('');
 const passwordMaterials = {
-    uppercase:'abcdefghijklmnopqrstuvwxyz'.split(''),
-    lowercase:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-    numbers:'0123456789'.split(''),
-    symbols:'!@#$%^&*()_+[]{}|;:,.<>?'.split(''),
-}
-function detectPasswordLength(){
-    characterLengthContainer.textContent = inputRange.value;
-    passwordLength = Number(inputRange.value);
+  uppercase: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  lowercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+  numbers: '0123456789'.split(''),
+  symbols: '!@#$%^&*()_+[]{}|;:,.<>?'.split(''),
 };
-
-function generatePassword(){
-    //generate initial password based on checkbox statuses 
-    //add random symbols till password reaches passwordLength
-    //link checked checkboxed to sets defined in initial variables then construct central variable from which will be consturcted passwrod
-    let password = '';
-    const chosenPasswordMaterials = []
-    checkBoxes.forEach((checkBox)=>{
-        if(checkBox.checked) chosenPasswordMaterials.push(passwordMaterials[checkBox.dataset.checkboxtype]);
-    });
-    console.log(chosenPasswordMaterials)
-    for(let i = 0; i < chosenPasswordMaterials.length; i++){
-        const array = new Uint8Array(chosenPasswordMaterials.length);  
-        const randomValues = crypto.getRandomValues(array);
-        const randomValue = randomValues[0] / 255;
-        const randomIndex =  Math.floor(randomValue * (passwordLength - 1 + 1) + 1);
-        const passwordMaterialSet = chosenPasswordMaterials[i];
-        password+=passwordMaterialSet[randomIndex]
-    }
-    console.log(password)
-    return password
+function detectPasswordLength() {
+  characterLengthContainer.textContent = inputRange.value;
+  passwordLength = Number(inputRange.value);
 }
-inputRange.addEventListener('change',detectPasswordLength);
-generateButton.addEventListener('click',generatePassword)
+
+function generatePassword() {
+  //generate initial password based on checkbox statuses
+  //add random symbols till password reaches passwordLength
+  //link checked checkboxed to sets defined in initial variables then construct central variable from which will be consturcted passwrod
+ 
+  let password = '';
+  const chosenPasswordMaterials = [];
+  checkBoxes.forEach((checkBox) => {
+    if (checkBox.checked)
+      chosenPasswordMaterials.push(
+        passwordMaterials[checkBox.dataset.checkboxtype]
+      );
+  });
+  if (chosenPasswordMaterials.length === 0) {
+    alert('Include at least one character set');
+    return;
+  }
+  for (let i = 0; i < chosenPasswordMaterials.length; i++) {
+    const array = new Uint8Array(chosenPasswordMaterials.length);
+    const randomValues = crypto.getRandomValues(array);
+    const randomValue = randomValues[0] / 255;
+    const passwordMaterialSet = chosenPasswordMaterials[i];
+    const randomIndex = Math.floor(randomValue * ((passwordMaterialSet.length-2) - 1 + 1) + 1);
+    console.log(passwordMaterialSet,randomIndex)
+    password += passwordMaterialSet[randomIndex];
+  }
+  console.log(password)
+  return password;
+}
+inputRange.addEventListener('change', detectPasswordLength);
+generateButton.addEventListener('click', generatePassword);
 detectPasswordLength();
