78da50e4eb7d0276319a4d8b9842f544b3211ff8
nk-on <2nkoz@proton.me>
2024-10-15 21:19:50 -0700

    Adding different text inside level container for different types of password strength

--- src/levelindicators.js
+++ src/levelindicators.js
@@ -1,26 +1,33 @@
 import levelIndicators from "./script.js";
-function colorizeLevelIndicators(levels,color){
+const levelContainer = document.querySelector('.level-container');
+function displayLevel(color,level){
+  levelContainer.textContent = level;
+  levelContainer.style.color = color;
+}
+function colorizeLevelIndicators(levels,color,level){
     for(let i = 0; i < levels; i++){
       levelIndicators[i].style.backgroundColor = color;
     }
     for(let i = levels; i < levelIndicators.length; i++){
      levelIndicators[i].style.backgroundColor = ' #08070B';
     }
+    console.log(color,level)
+    displayLevel(color,level);
  }
  function evaluateStrength(passwordStrength){
    console.log(passwordStrength)
    switch(passwordStrength){
      case 0:
-       colorizeLevelIndicators(passwordStrength+1,'#F64A4A;');
+       colorizeLevelIndicators(passwordStrength+1,'#F64A4A','Too weak');
        break;
      case 1:
-       colorizeLevelIndicators(passwordStrength+1,'#FB7C58');
+       colorizeLevelIndicators(passwordStrength+1,'#FB7C58','Weak');
        break;
      case 2:
-       colorizeLevelIndicators(passwordStrength+1,'#F8CD65');
+       colorizeLevelIndicators(passwordStrength+1,'#F8CD65','Medium');
        break;
      default:
-       colorizeLevelIndicators(levelIndicators.length,'#A4FFAF');
+       colorizeLevelIndicators(levelIndicators.length,'#A4FFAF','Strong');
    }
  }
  export default evaluateStrength
